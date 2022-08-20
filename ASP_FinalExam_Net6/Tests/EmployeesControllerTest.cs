ef37f06ed90207415085deb0d69b08d01f29557d
Daniel Volchok <200389907@student.georgianc.on.ca>
2022-08-19 21:01:46 -0400

    Successful test on Employee Controller Index method

---
+++ ASP_FinalExam_Net6/Tests/EmployeesControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_FinalExam_Net6.Controllers;
+using ASP_FinalExam_Net6.Data;
+using ASP_FinalExam_Net6.Models;
+
+namespace ASP_FinalExam_Net6.Tests
+{
+    public class EmployeesControllerTest : InMemoryTest
+    {
+        [Fact]
+        public async Task Index_Returns_ViewResult_And_EmployeeList()
+        {
+            // disposes this test db after use
+            using (var testDb = new ApplicationDbContext(this._opts))
+            {
+                var testCtrl = new EmployeesController(testDb);
+                var result = await testCtrl.Index();
+                var resVr = Assert.IsType<ViewResult>(result);
+                Assert.IsAssignableFrom<IEnumerable<Employee>>(resVr.ViewData.Model);
+            }
+        }
+    }
+}
---
+++ ASP_FinalExam_Net6/Tests/InMemoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_FinalExam_Net6.Controllers;
+using ASP_FinalExam_Net6.Data;
+using ASP_FinalExam_Net6.Models;
+
+namespace ASP_FinalExam_Net6.Tests
+{
+    public class InMemoryTest
+    {
+        protected readonly DbContextOptions<ApplicationDbContext> _opts;
+
+        // generates connection
+        public InMemoryTest()
+        {
+            _opts = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Exam").Options;
+        }
+    }
+}
